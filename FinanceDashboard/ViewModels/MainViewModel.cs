a39d03d041d6c283c4d49de2cdce5c4fac02bfd3
EduardoCassanha <eduardocassanhaperalta@gmail.com>
2026-02-08 17:43:39 -0300

    feat: complete MVVM setup and fix view connection

---
+++ FinanceDashboard/ViewModels/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FinanceDashboard.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private string _appName = string.Empty;
+        private string _welcomeMessage = string.Empty;
+
+        public string AppName
+        {
+            get { return _appName; }
+            set
+            {
+                SetProperty(ref _appName, value);
+            }
+        }
+
+        public string WelcomeMessage
+        {
+            get { return _welcomeMessage; }
+            set
+            {
+                SetProperty(ref _welcomeMessage, value);
+            }
+
+        }
+
+        public MainViewModel()
+        {
+            AppName = "Finance Dashboard v1.0";
+            WelcomeMessage = "Welcome to your finance dashboard.";
+        }
+    }
+}
---
+++ FinanceDashboard/ViewModels/ViewModelBase.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace FinanceDashboard.ViewModels
+{
+    // Abstract prevents this class from being instantiated directly
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        // Helper method to safely raise the PropertyChanged event 
+        protected void OnPropertyChanged([CallerMemberName] string?  propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        // <T> is a generic method to update values
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string? propertyName = null)
+        {
+
+            // if the value is the same of the last one, return false
+            if (EqualityComparer<T>.Default.Equals(storage, value))
+            {
+                return false; 
+            }
+
+            // if changed, update variable
+            storage = value;
+
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
